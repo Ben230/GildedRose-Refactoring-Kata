4a3a89b25f4464be65e4d8d68c85298896848365
Ben Stuart <benarthurstuart@hotmail.com>
2019-12-20 12:08:36 +0000

    made isQualityPositive and changeQuality functions

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -13,12 +13,21 @@ class Shop {
   }
 
   updateQuality() {
+
+    // for (var i = 0; i < this.items.length; i++) {
+    //   if (this.isQualityPositive(this.items[i].quality) {
+    //
+    //     this.changeQuality(this.items[i], -1);
+    //   }
+    // }
+
+
     for (var i = 0; i < this.items.length; i++) {
       // refactor to jsut include backstage pass
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
+        if (this.isQualityPositive(this.items[i].quality)) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.changeQuality(this.items[i], -1);
           }
         }
       } else { // if aged bri *or* baxckstage passes
@@ -63,8 +72,73 @@ class Shop {
 
     return this.items;
   }
+
+  isQualityPositive(quality) {
+    if (quality > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  changeQuality(item, amount) {
+    item.quality += amount;
+  }
+
 }
+
 module.exports = {
   Item,
   Shop
 }
+
+// updateQuality() {
+//   for (var i = 0; i < this.items.length; i++) {
+//     // refactor to jsut include backstage pass
+//     if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+//       if (this.items[i].quality > 0) {
+//         if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+//           this.items[i].quality = this.items[i].quality - 1;
+//         }
+//       }
+//     } else { // if aged bri *or* baxckstage passes
+//       if (this.items[i].quality < 50) {
+//         this.items[i].quality = this.items[i].quality + 1;
+//         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+//           if (this.items[i].sellIn < 11) {
+//             if (this.items[i].quality < 50) {
+//               this.items[i].quality = this.items[i].quality + 1;
+//             }
+//           }
+//           if (this.items[i].sellIn < 6) {
+//             if (this.items[i].quality < 50) {
+//               this.items[i].quality = this.items[i].quality + 1;
+//             }
+//           }
+//         }
+//       }
+//     }
+//     // refactor to just include Sulfuras
+//     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+//       this.items[i].sellIn = this.items[i].sellIn - 1;
+//     }
+//     if (this.items[i].sellIn < 0) {
+//       if (this.items[i].name != 'Aged Brie') {
+//         if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+//           if (this.items[i].quality > 0) {
+//             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+//               this.items[i].quality = this.items[i].quality - 1;
+//             }
+//           }
+//         } else {
+//           this.items[i].quality = this.items[i].quality - this.items[i].quality;
+//         }
+//       } else {
+//         if (this.items[i].quality < 50) {
+//           this.items[i].quality = this.items[i].quality + 1;
+//         }
+//       }
+//     }
+//   }
+//
+//   return this.items;
+// }
